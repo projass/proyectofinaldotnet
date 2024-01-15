c933962a32edee01d0560b52c316d72b29c0508e
iEcclMyt <eloydmp@gmail.com>
2024-01-15 09:21:42 +0100

    Actualización 9

---
+++ Proyecto Final - Adapta-Tech/src/app/prueba/prueba.component.ts
@@ -0,0 +1,13 @@
+import { Component } from '@angular/core';
+import { RouterModule } from '@angular/router';
+
+@Component({
+  selector: 'app-prueba',
+  standalone: true,
+  imports: [RouterModule],
+  templateUrl: './prueba.component.html',
+  styleUrl: './prueba.component.css'
+})
+export class PruebaComponent {
+
+}
