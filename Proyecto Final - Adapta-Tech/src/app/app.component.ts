1b3de4d288be68654be1809528bff8b7b342d4a8
iEcclMyt <eloydmp@gmail.com>
2024-01-15 10:01:18 +0100

    prueba

--- Proyecto Final - Adapta-Tech/src/app/app.component.ts
+++ Proyecto Final - Adapta-Tech/src/app/app.component.ts
@@ -3,13 +3,14 @@ import { CommonModule } from '@angular/common';
 import { RouterOutlet } from '@angular/router';
 import { MenuComponent } from "./menu/menu/menu.component";
 import { FooterComponent } from './menu/footer/footer.component';
+import { ColaboradoresComponent } from './Colaboradores/colaboradores.component';
 
 @Component({
     selector: 'app-root',
     standalone: true,
     templateUrl: './app.component.html',
     styleUrl: './app.component.css',
-    imports: [CommonModule, RouterOutlet, MenuComponent, FooterComponent]
+    imports: [CommonModule, RouterOutlet, MenuComponent, FooterComponent, ColaboradoresComponent]
 })
 export class AppComponent {
   title = 'MiProyecto';
--- Proyecto Final - Adapta-Tech/src/app/menu/menu/menu.component.ts
+++ Proyecto Final - Adapta-Tech/src/app/menu/menu/menu.component.ts
@@ -1,12 +1,13 @@
 import { Component } from '@angular/core';
 import { RouterModule } from '@angular/router';
+import { ColaboradoresComponent } from '../../Colaboradores/colaboradores.component';
 
 @Component({
     selector: 'app-menu',
     standalone: true,
     templateUrl: './menu.component.html',
     styleUrl: './menu.component.css',
-    imports: [RouterModule]
+    imports: [RouterModule, ColaboradoresComponent]
 })
 export class MenuComponent{
  
