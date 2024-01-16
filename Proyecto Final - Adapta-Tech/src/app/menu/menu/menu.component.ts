9341a7733e208c5151360e2947d2d3e5b1b9f165
iEcclMyt <eloydmp@gmail.com>
2024-01-16 21:32:33 +0100

    Actualización con una hamburguesa mal diseñada

--- Proyecto Final - Adapta-Tech/src/app/menu/menu/menu.component.ts
+++ Proyecto Final - Adapta-Tech/src/app/menu/menu/menu.component.ts
@@ -1,4 +1,4 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 import { RouterModule } from '@angular/router';
 
 @Component({
@@ -8,7 +8,29 @@ import { RouterModule } from '@angular/router';
     styleUrl: './menu.component.css',
     imports: [RouterModule]
 })
-export class MenuComponent{
- 
+export class MenuComponent implements OnInit{
+    nav: HTMLElement | null = null;
+    abrir: HTMLElement | null = null;
+    cerrar: HTMLElement | null = null;
+
+    constructor() {}
+
+    ngOnInit() {
+        this.nav = document.querySelector("#nav");
+        this.abrir = document.querySelector("#abrir");
+        this.cerrar = document.querySelector("#cerrar");
+
+        this.abrir?.addEventListener("click", () => {
+            if (this.nav?.classList) {
+                this.nav.classList.add("visible");
+            }
+        });
+
+        this.cerrar?.addEventListener("click", () => {
+            if (this.nav?.classList) {
+                this.nav.classList.remove("visible");
+            }
+        });
+    }
 }
 
