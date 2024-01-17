df808ad5fc7c9c9681eb6ac25eca2e40e8e6382d
iEcclMyt <eloydmp@gmail.com>
2024-01-18 00:22:45 +0100

    Actualización 12

--- Proyecto Final - Adapta-Tech/src/app/Recursos/ayudas/ayudas.component.ts
+++ Proyecto Final - Adapta-Tech/src/app/Recursos/ayudas/ayudas.component.ts
@@ -1,9 +1,10 @@
 import { Component } from '@angular/core';
+import { RouterModule } from '@angular/router';
 
 @Component({
   selector: 'app-ayudas',
   standalone: true,
-  imports: [],
+  imports: [RouterModule],
   templateUrl: './ayudas.component.html',
   styleUrl: './ayudas.component.css'
 })
--- Proyecto Final - Adapta-Tech/src/app/menu/footer/footer.component.ts
+++ Proyecto Final - Adapta-Tech/src/app/menu/footer/footer.component.ts
@@ -1,11 +1,10 @@
 import { Component } from '@angular/core';
 import { RouterModule } from '@angular/router';
-import { ContactoComponent } from '../../contacto/contacto.component';
 
 @Component({
   selector: 'app-footer',
   standalone: true,
-  imports: [RouterModule,ContactoComponent],
+  imports: [RouterModule],
   templateUrl: './footer.component.html',
   styleUrl: './footer.component.css'
 })
