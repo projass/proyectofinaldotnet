d54ebcf2296245cbbecd37e9d0256eb3b30cf6a9
Sycmes <sycmes@gmail.com>
2024-01-29 13:21:58 +0100

    Actualizacion35

---
+++ Proyecto Final - Adapta-Tech/src/app/descargas/descargas.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-descargas',
+  standalone: true,
+  imports: [],
+  templateUrl: './descargas.component.html',
+  styleUrl: './descargas.component.css'
+})
+export class DescargasComponent {
+
+}
