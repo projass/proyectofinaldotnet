256f5f0a3d9997f700a0b7c6f02f5a8579714ff2
iEcclMyt <eloydmp@gmail.com>
2024-02-01 11:59:08 +0100

    Miguelnando

--- Proyecto Final - Adapta-Tech/src/app/menu/login/logueado/logueado.component.ts
+++ Proyecto Final - Adapta-Tech/src/app/menu/login/logueado/logueado.component.ts
@@ -14,14 +14,11 @@ export class LogueadoComponent implements OnInit{
   constructor(private loginService: LoginService) { }
 
   ngOnInit(): void {
-    // Llama al método de LoginService para obtener la información del perfil del usuario
     this.obtenerPerfilUsuario();
   }
 
   obtenerPerfilUsuario(): void {
-    // Verifica si el usuario está autenticado
     if (this.loginService.estaAutenticado()) {
-      // Obtiene la información del perfil del usuario
       const usuario = this.loginService.obtenerUsuario();
       this.email = usuario ? usuario.email : '';
     }
